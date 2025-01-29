1fac6062e9ab04a861bb5f2cd03f6ff1ba47c748
TLR1000 <oostrijck@gmail.com>
2025-01-29 10:18:08 +0100

    Adapted for Ender firmware bug

--- Ender3V3KEAutoThumbnail.py
+++ Ender3V3KEAutoThumbnail.py
@@ -1,73 +1,53 @@
-# Automatically include thumbnails in G-code 
-# Tested with Cura 5.9 and Creality Ender 3 V3 KE
-# Based on:
-# https://github.com/Ultimaker/Cura/blob/master/plugins/PostProcessingPlugin/scripts/CreateThumbnail.py
-
 import base64
-from UM.Logger import Logger
 from cura.Snapshot import Snapshot
 from PyQt6.QtCore import QBuffer
 from ..Script import Script
 
-
 class Ender3V3KEAutoThumbnail(Script):
     def __init__(self):
         super().__init__()
-
-    def _createSnapshot(self, width, height):
-        Logger.log("d", f"Creating thumbnail image ({width}x{height})...")
-        try:
-            return Snapshot.snapshot(width, height)
-        except Exception:
-            Logger.logException("w", f"Failed to create snapshot image ({width}x{height})")
-
-    def _encodeSnapshot(self, snapshot):
-        Logger.log("d", "Encoding thumbnail image...")
-        try:
-            thumbnail_buffer = QBuffer()
-            thumbnail_buffer.open(QBuffer.OpenModeFlag.ReadWrite)
-            snapshot.save(thumbnail_buffer, "PNG")
-            thumbnail_data = thumbnail_buffer.data()
-            base64_bytes = base64.b64encode(thumbnail_data)
-            base64_message = base64_bytes.decode('ascii')
-            thumbnail_buffer.close()
-            Logger.log("d", f"Snapshot encoded, length={len(thumbnail_data)}")
-            return base64_message, len(thumbnail_data)
-        except Exception:
-            Logger.logException("w", "Failed to encode snapshot image")
-
-    def _convertSnapshotToGcode(self, thumbnail_length, encoded_snapshot, width, height, chunk_size=76):
-        Logger.log("d", f"Converting {width}x{height} snapshot into G-code...")
-        gcode = []
-
-        header = f"; THUMBNAIL_BEGIN {width}x{height} {thumbnail_length}"
-        Logger.log("d", f"Gcode header={header}")
-        gcode.append(header)
-
-        chunks = ["; " + encoded_snapshot[i:i + chunk_size]
-                  for i in range(0, len(encoded_snapshot), chunk_size)]
-        gcode.extend(chunks)
-
-        gcode.append("; THUMBNAIL_END")
-        gcode.append(";")
-
-        return gcode
+    
+    def generate_thumbnail(self, width, height):
+        """
+        Genereert een PNG-snapshot van het huidige model en zet deze om in base64.
+        """
+        snapshot = Snapshot.snapshot(width, height)
+        if not snapshot:
+            raise ValueError("Kon geen snapshot genereren.")
+        
+        thumbnail_buffer = QBuffer()
+        thumbnail_buffer.open(QBuffer.OpenModeFlag.ReadWrite)
+        snapshot.save(thumbnail_buffer, "PNG")
+        thumbnail_data = thumbnail_buffer.data()
+        base64_bytes = base64.b64encode(thumbnail_data)
+        base64_message = base64_bytes.decode('ascii')
+        thumbnail_buffer.close()
+        
+        return base64_message, len(thumbnail_data)
 
     def execute(self, data):
-        Logger.log("d", "Ender3V3KEAutoThumbnail Plugin start")
-
-        # Automatisch 96x96 en 300x300 thumbnails genereren
-        for width, height in [(96, 96), (300, 300)]:
-            snapshot = self._createSnapshot(width, height)
-            if snapshot:
-                Logger.log("d", f"Snapshot {width}x{height} created")
-                encoded_snapshot, thumbnail_length = self._encodeSnapshot(snapshot)
-                snapshot_gcode = self._convertSnapshotToGcode(thumbnail_length, encoded_snapshot, width, height)
-
-                if len(data) > 0:
-                    # Thumbnails toevoegen aan het begin van de G-code
-                    data[0] = "\n".join(snapshot_gcode) + "\n" + data[0]
-                    Logger.log("d", f"Added {width}x{height} snapshot G-code")
-
-        Logger.log("d", "Ender3V3KEAutoThumbnail Plugin end")
+        """
+        Genereert en injecteert meerdere thumbnails (96x96 en 300x300) in de G-code,
+        aangepast voor Creality Ender-3 compatibiliteit.
+        """
+        resolutions = [(96, 96), (300, 300)]
+        thumbnails = []
+        
+        for width, height in resolutions:
+            encoded_snapshot, thumbnail_size = self.generate_thumbnail(width, height)
+            base64_lines = [encoded_snapshot[i:i+76] for i in range(0, len(encoded_snapshot), 76)]
+            
+            # Creality Ender-3 compatibele thumbnail headers
+            thumbnail_section = [
+                f"; png begin {width} * {height} {thumbnail_size} 0 95 160\n"
+            ] + [f"; {line}\n" for line in base64_lines] + [
+                "; png end\n"
+            ]
+            
+            thumbnails.extend(thumbnail_section)
+        
+        # Injecteer thumbnails aan het begin van de G-code
+        if len(data) > 0:
+            data[0] = "\n".join(thumbnails) + "\n" + data[0]
+        
         return data
